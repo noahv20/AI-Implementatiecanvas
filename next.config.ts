284498a13d4814892b42e19fcecefa0b6389fb44
noahv20 <165033533+noahv20@users.noreply.github.com>
2026-03-17 10:36:20 +0100

    fix: names

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const repoName = "Ai-Implementatiecanvas";
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
   images: {
     unoptimized: true,
   },
